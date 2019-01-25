8b448d3ffc28ae17c28e6e2aa78b215807b1f866
Rodrigo F. Fernandes <rodrigoff@live.com>
2019-01-25 13:48:37 -0200

    Initial Commit

---
+++ EventKitSpike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventKitSpike
+//
+//  Created by Rodrigo F. Fernandes on 25/01/19.
+//  Copyright © 2019 Rodrigo F. Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
